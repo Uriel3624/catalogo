05bc929e7aa84aaae38964ca59aeab804f60713e
Uriel3624 <facu.uriel.2002@gmail.com>
2021-11-30 16:58:00 -0300

    catalogo

---
+++ catalogo/catalogo/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['midominicio.com.ar']
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
